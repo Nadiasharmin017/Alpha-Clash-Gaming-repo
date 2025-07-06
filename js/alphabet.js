abcc28562561e8133c418f87b907d25285c95a8e
Nadiasharmin017 <nadiasharmin017@gmail.com>
2025-07-06 14:59:12 +0600

    set the background colour

--- js/alphabet.js
+++ js/alphabet.js
@@ -3,13 +3,13 @@ function getRandomAlphabet(){
     const alphabetString = "abcdefghijklmnopqrstuvwxyz";
     //split the string into an array of characters
     const alphabets = alphabetString.split("")
-        console.log(alphabets)
+        // console.log(alphabets)
 
 
     //get a random index between 0-25
     const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
-    console.log(index);
+    // console.log(index);
     const alphabet = alphabets[index];
     console.log(index,alphabet);
     return alphabet;
--- js/style.js
+++ js/style.js
@@ -1,6 +1,14 @@
+//alphabet continue game
 function continueGame(){
     const alpha = getRandomAlphabet();
+    console.log("Random alphabet is: ",alpha)
+    //show the alphabet on the screen
+    const currentAlphabet = document.getElementById("current-alphabet");
+    currentAlphabet.innerText = alpha;
+    setBackgoundColor(alpha);
 }
+
+//play now click button
 function playNow(){
     // console.log("c;ick baba")
     //step1- hide the home screen
@@ -12,6 +20,13 @@ function playNow(){
     const playGround = document.getElementById("playground");
     // console.log(palyGround.classList)
     //remove hidden on the classlist
-    playGround.classList.remove("hidden")
-    continueGame()
+    playGround.classList.remove("hidden");
+    continueGame();
+  
+}
+//set backgound color
+function setBackgoundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add("bg-orange-400");
+
 }
